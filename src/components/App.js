060b29c3969e1c72d7392b023feb5ce4b07e66da
logx <eskejpth@gmail.com>
2018-03-26 20:43:46 +0200

    Color component created

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../logo.svg";
+import Color from "./Color";
 import "../App.css";
 
 class App extends React.Component {
@@ -8,7 +9,6 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Color.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Color extends React.Component {
+  render() {
+    return <p>Some colored text</p>;
+  }
+}
+export default Color;
