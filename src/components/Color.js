c9da0907bf93bcc7384c99e6578b29b93b82f0af
logx <eskejpth@gmail.com>
2018-03-26 21:33:09 +0200

    fetch api added and color change handled

--- src/components/Color.js
+++ src/components/Color.js
@@ -7,11 +7,24 @@ class Color extends React.Component {
     text: "Some colored text"
   };
   //handle the element change
-
+  colorChange = event => {
+    //check the current color to add the color toggle functionality
+    if (this.state.color === "#000") {
+      //get the new color from API endpoint
+      fetch("http://www.colr.org/json/color/random")
+        .then(result => result.json())
+        .then(data => {
+          let newColor = data.colors[0].hex;
+          this.setState({ color: "#" + newColor });
+        });
+    } else {
+      this.setState({ color: "#000" });
+    }
+  };
   render() {
     return (
       <div>
-        <p>{this.state.text}</p>
+        <p onClick={this.colorChange}>{this.state.text}</p>
       </div>
     );
   }
